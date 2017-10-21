7811b986117d1be24c3fcad03c6f4770b311727a
kimxogus <kgyoo8232@gmail.com>
2017-10-21 15:03:06 +0900

    update json-injector, silent option to cli

--- packages/json-injector-cli/src/app.js
+++ packages/json-injector-cli/src/app.js
@@ -4,7 +4,7 @@ import yargs from 'yargs';
 import jsonInjector from 'json-injector';
 import { defaultOptions } from 'json-injector/lib/schema/options';
 
-const { _: files = defaultOptions.files, suffix, file, verbose } = yargs
+const { _: files = defaultOptions.files, suffix, file, verbose, silent } = yargs
   .option('file', {
     alias: 'f',
     default: defaultOptions.rcFile,
@@ -16,6 +16,10 @@ const { _: files = defaultOptions.files, suffix, file, verbose } = yargs
   .option('verbose', {
     alias: 'v',
     default: defaultOptions.verbose,
+  })
+  .option('silent', {
+    alias: 's',
+    default: defaultOptions.silent,
   }).argv;
 
 jsonInjector({ files, suffix, rcFile: file, verbose });
